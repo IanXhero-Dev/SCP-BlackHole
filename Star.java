ff7441625538820d6d4c3e9eb2872d2043e6154f
Ian <ian.quiero@alumnos.uv.cl>
2022-10-25 20:14:00 -0300

    Creacion archivos del algoritmo

---
+++ Star.java
@@ -0,0 +1,15 @@
+public class Star {
+    // Atributos
+    private int dimension;
+    private int fitness;
+
+    // Metodos
+    public void setDimension() {
+
+    }
+
+    public void setFitness() {
+
+    }
+
+}
